dec01224157e942cb28eb626fd87f1982ca3bba4
Papazxd <76903856+Papazxd@users.noreply.github.com>
2022-07-04 23:02:31 +0300

    Update yardım.js

--- src/commands/Kayıt/yardım.js
+++ src/commands/Kayıt/yardım.js
@@ -11,18 +11,10 @@ module.exports = {
 \`- ${config.bot.prefix}kke [@Kişi/ID]
 - ${config.bot.prefix}isimler [@Kişi/ID]
 - ${config.bot.prefix}kayıtsız-etiketle
-- ${config.bot.prefix}ping
 - ${config.bot.prefix}kayıt [@Kişi/ID] (isim-yaş)
 - ${config.bot.prefix}isim [@Kişi/ID] (isim-yaş)
 - ${config.bot.prefix}kayıtsız
 - ${config.bot.prefix}top-kayıt [@Kişi/ID]
-- ${config.bot.prefix}müzisyen [@Kişi/ID]
-- ${config.bot.prefix}sponsor [@Kişi/ID]
-- ${config.bot.prefix}vip [@Kişi/ID]
-- ${config.bot.prefix}veri-sıfırla [@Kişi/ID]
-- ${config.bot.prefix}eval (kod)
-- ${config.bot.prefix}rolsüz (ver)
-- ${config.bot.prefix}reboot\`
 `)] }).catch((err) => console.log(err), client.ytick(message)).then((e) => setTimeout(() => { e.delete(); }, 60000));
 
     }
